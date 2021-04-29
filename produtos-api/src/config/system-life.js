6240781a84327607cf0a8674e2bf68562e9b7310
Rodrigo Arantes <rodriggoarantes@gmail.com>
2021-04-29 18:35:39 -0300

    gerando nova versao v2

--- produtos-api/src/config/system-life.js
+++ produtos-api/src/config/system-life.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 let isHealth = true;
 let readTime = new Date(Date.now());
-let isRead = () => { 
+let isRead = () => {
     return readTime < new Date(Date.now());
 };
 
 router.get('/read', (req, res) => {
-   
+
     if (isRead()) {
         res.statusCode = 200;
-        return res.send('Ok');
+        return res.send('Ok - v2');
     } else {
         res.statusCode = 500;
         return res.send('');
-    }   
+    }
 });
 
 router.put('/unhealth', (req, res) => {
@@ -25,21 +25,21 @@ router.put('/unhealth', (req, res) => {
 });
 
 router.put('/unreadfor/:seconds', (req, res) => {
-    
+
     const dado = new Date(new Date(Date.now()).getTime() + (1000 * req.params.seconds));
-    readTime = dado;    
+    readTime = dado;
     res.send("OK");
 });
 
 var healthMid = function (req, res, next) {
-    
+
     if (isHealth) {
         next();
     } else {
         res.statusCode = 500;
         return res.send('');
-    }   
+    }
 };
 
 exports.routers = router;
-exports.middlewares = { healthMid};
+exports.middlewares = { healthMid };
